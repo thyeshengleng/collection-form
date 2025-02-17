c4508cafc346538860a06015dec0233316bd3831
lengthyesheng0721 <your-email@gmail.com>
2025-02-17 15:56:56 +0800

    initial commit

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def render_db_form():
         try:
             # Create direct ODBC connection
             conn_str = (
-                'DRIVER={SQL Server};'
+                'DRIVER={ODBC Driver 17 for SQL Server};'
                 'SERVER=DESKTOP-RMNV9QV\\A2006;'
                 'DATABASE=AED_AssignmentOne;'
                 'UID=sa;'
