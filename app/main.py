980d7f7b13739b518b80b016a9d6299fa0561a0e
lengthyesheng0721 <your-email@gmail.com>
2025-02-21 10:14:59 +0800

    initial commit

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ def render_db_form():
             # Create SQLAlchemy connection string
             params = urllib.parse.quote_plus(
                 'DRIVER={ODBC Driver 17 for SQL Server};'
-                'SERVER=collection-form-u3uosjedoabyvvmr9pwfoa.streamlit.app;'
+                'SERVER=DESKTOP-RMNV9QV\\A2006;'
                 'DATABASE=AED_AssignmentOne;'
                 'UID=sa;'
                 'PWD=oCt2005-ShenZhou6_A2006;'
