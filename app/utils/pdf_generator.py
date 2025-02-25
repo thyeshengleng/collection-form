7558aace9fefd3098777374244ac03c20338533f
lengthyesheng0721 <your-email@gmail.com>
2025-02-25 16:12:17 +0800

    initial commit

---
+++ app/utils/pdf_generator.py
@@ -0,0 +1,138 @@
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
+from datetime import datetime
+
+def generate_pdf(data, output_path):
+    # Create the PDF document
+    doc = SimpleDocTemplate(
+        output_path,
+        pagesize=A4,
+        rightMargin=72,
+        leftMargin=72,
+        topMargin=72,
+        bottomMargin=72
+    )
+
+    # Container for the 'Flowable' objects
+    elements = []
+    
+    # Get styles
+    styles = getSampleStyleSheet()
+    title_style = ParagraphStyle(
+        'CustomTitle',
+        parent=styles['Heading1'],
+        fontSize=16,
+        spaceAfter=30,
+        alignment=1  # Center alignment
+    )
+    
+    # Add title
+    elements.append(Paragraph("Job Order Details", title_style))
+    elements.append(Spacer(1, 12))
+    
+    # Company Information section
+    company_data = [
+        ['Company Information', ''],
+        ['Company Name:', data.get('Company Name', '')],
+        ['Email:', data.get('Email', '')],
+        ['Address:', data.get('Address', '')],
+        ['Business Info:', data.get('Business Info', '')],
+        ['Tax ID:', data.get('Tax ID', '')]
+    ]
+    
+    company_table = Table(company_data, colWidths=[2*inch, 4*inch])
+    company_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+        ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+        ('FONTSIZE', (0, 1), (-1, -1), 10),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    elements.append(company_table)
+    elements.append(Spacer(1, 20))
+    
+    # Module Information section
+    module_data = [
+        ['Module Information', ''],
+        ['Plug In Module:', data.get('Plug In Module', '')],
+        ['Module & User License:', data.get('Module & User License', '')],
+        ['VPN Info:', data.get('VPN Info', '')]
+    ]
+    
+    module_table = Table(module_data, colWidths=[2*inch, 4*inch])
+    module_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+        ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+        ('FONTSIZE', (0, 1), (-1, -1), 10),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    elements.append(module_table)
+    elements.append(Spacer(1, 20))
+    
+    # Report and Migration Information section
+    report_data = [
+        ['Report and Migration Details', ''],
+        ['Report Design Template:', data.get('Report Design Template', '')],
+        ['Migration Master Data:', data.get('Migration Master Data', '')],
+        ['Migration Outstanding Balance:', data.get('Migration Outstanding Balance', '')]
+    ]
+    
+    report_table = Table(report_data, colWidths=[2*inch, 4*inch])
+    report_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+        ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+        ('FONTSIZE', (0, 1), (-1, -1), 10),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    elements.append(report_table)
+    elements.append(Spacer(1, 20))
+    
+    # Status Information
+    status_data = [
+        ['Status Information', ''],
+        ['Current Status:', data.get('Status', '')],
+        ['E-Invoice Start Date:', data.get('E-Invoice Start Date', '')]
+    ]
+    
+    status_table = Table(status_data, colWidths=[2*inch, 4*inch])
+    status_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+        ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+        ('FONTSIZE', (0, 1), (-1, -1), 10),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    elements.append(status_table)
+    
+    # Generate the PDF
+    doc.build(elements)
+    return output_path
