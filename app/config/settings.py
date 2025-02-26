911d073147011916dd69157d52b3c16b55d33b00
lengthyesheng0721 <your-email@gmail.com>
2025-02-26 11:10:30 +0800

    initial commit

--- app/config/settings.py
+++ app/config/settings.py
@@ -19,35 +19,35 @@ REPORT_OPTIONS = [
     "GST Report"
 ]
 
-# Implementation options
-IMPLEMENTATION_OPTIONS = [
-    "SALES ORDER",
-    "DELIVERY ORDER",
-    "SALES INVOICE",
-    "PICKING LIST",
-    "PURCHASE ORDER",
-    "CASH SALES",
-    "QUOTATION",
-    "OFFICIAL RECEIPT",
-    "PAYMENT VOUCHER"
-]
+# # Implementation options
+# IMPLEMENTATION_OPTIONS = [
+#     "SALES ORDER",
+#     "DELIVERY ORDER",
+#     "SALES INVOICE",
+#     "PICKING LIST",
+#     "PURCHASE ORDER",
+#     "CASH SALES",
+#     "QUOTATION",
+#     "OFFICIAL RECEIPT",
+#     "PAYMENT VOUCHER"
+# ]
 
-# Master data options
-MASTER_DATA_OPTIONS = [
-    "CHART OF ACCOUNT",
-    "DEBTOR",
-    "CREDITOR",
-    "STOCK ITEM"
-]
+# # Master data options
+# MASTER_DATA_OPTIONS = [
+#     "CHART OF ACCOUNT",
+#     "DEBTOR",
+#     "CREDITOR",
+#     "STOCK ITEM"
+# ]
 
-# Migration options
-MIGRATION_OPTIONS = [
-    "DEBTOR AGING",
-    "CREDITOR AGING",
-    "TRIAL BALANCE REPORT",
-    "BALANCE SHEET REPORT",
-    "STOCK BALANCE",
-    "SO/PO OUTSTANDING"
-]
+# # Migration options
+# MIGRATION_OPTIONS = [
+#     "DEBTOR AGING",
+#     "CREDITOR AGING",
+#     "TRIAL BALANCE REPORT",
+#     "BALANCE SHEET REPORT",
+#     "STOCK BALANCE",
+#     "SO/PO OUTSTANDING"
+# ]
 
 STATUS_OPTIONS = ["pending", "complete", "AR/Ap, Stock pending"]
